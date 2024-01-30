a1e9f315a6d2e198ba90c63f0b72c275eeaaec7b
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2024-01-30 12:11:20 +0100

    Add feedback

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,37 @@
-import os
 from pathlib import Path
 
 import pytest
 
 
-def absolute_path(filename: str) -> str:
-    return os.path.join(os.path.dirname(__file__), filename)
+def absolute_path(filename: str) -> Path:
+    return Path(__file__).parent.joinpath(filename)
 
 
 @pytest.fixture
 def xp_modified_remote_lnk_file() -> Path:
-    return Path(absolute_path("data/modified_remote.file.xp.lnk"))
+    return absolute_path("data/modified_remote.file.xp.lnk")
 
 
 @pytest.fixture
 def xp_remote_lnk_file() -> Path:
-    return Path(absolute_path("data/remote.file.xp.lnk"))
+    return absolute_path("data/remote.file.xp.lnk")
 
 
 @pytest.fixture
 def xp_remote_lnk_dir() -> Path:
-    return Path(absolute_path("data/remote.directory.xp.lnk"))
+    return absolute_path("data/remote.directory.xp.lnk")
 
 
 @pytest.fixture
 def win7_local_lnk_dir() -> Path:
-    return Path(absolute_path("data/local.directory.seven.lnk"))
+    return absolute_path("data/local.directory.seven.lnk")
 
 
 @pytest.fixture
 def win81_downloads_lnk_dir() -> Path:
-    return Path(absolute_path("data/downloads.win81.lnk"))
+    return absolute_path("data/downloads.win81.lnk")
 
 
 @pytest.fixture
 def vista_idlist_lnk_file() -> Path:
-    return Path(absolute_path("data/vista.idlist.lnk"))
+    return absolute_path("data/vista.idlist.lnk")
