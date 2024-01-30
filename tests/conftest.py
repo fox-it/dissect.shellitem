38ae64ea20f3c42459eaa53ccb01514cbe8d69e8
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2024-01-30 09:44:10 +0100

    Add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,30 +4,35 @@ from pathlib import Path
 import pytest
 
 
-def absolute_path(filename):
+def absolute_path(filename: str) -> str:
     return os.path.join(os.path.dirname(__file__), filename)
 
 
 @pytest.fixture
-def xp_modified_remote_lnk_file():
+def xp_modified_remote_lnk_file() -> Path:
     return Path(absolute_path("data/modified_remote.file.xp.lnk"))
 
 
 @pytest.fixture
-def xp_remote_lnk_file():
+def xp_remote_lnk_file() -> Path:
     return Path(absolute_path("data/remote.file.xp.lnk"))
 
 
 @pytest.fixture
-def xp_remote_lnk_dir():
+def xp_remote_lnk_dir() -> Path:
     return Path(absolute_path("data/remote.directory.xp.lnk"))
 
 
 @pytest.fixture
-def win7_local_lnk_dir():
+def win7_local_lnk_dir() -> Path:
     return Path(absolute_path("data/local.directory.seven.lnk"))
 
 
 @pytest.fixture
-def win81_downloads_lnk_dir():
+def win81_downloads_lnk_dir() -> Path:
     return Path(absolute_path("data/downloads.win81.lnk"))
+
+
+@pytest.fixture
+def vista_idlist_lnk_file() -> Path:
+    return Path(absolute_path("data/vista.idlist.lnk"))
