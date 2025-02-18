eb6c5b47eccabab62405ede40ec23f3e19c70860
Computer Network Investigation <121175071+JSCU-CNI@users.noreply.github.com>
2025-02-18 17:21:21 +0100

    Fix Sphinx rst warnings (#39)

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,29 +11,29 @@ def absolute_path(filename: str) -> Path:
 
 @pytest.fixture
 def xp_modified_remote_lnk_file() -> Path:
-    return absolute_path("data/modified_remote.file.xp.lnk")
+    return absolute_path("_data/modified_remote.file.xp.lnk")
 
 
 @pytest.fixture
 def xp_remote_lnk_file() -> Path:
-    return absolute_path("data/remote.file.xp.lnk")
+    return absolute_path("_data/remote.file.xp.lnk")
 
 
 @pytest.fixture
 def xp_remote_lnk_dir() -> Path:
-    return absolute_path("data/remote.directory.xp.lnk")
+    return absolute_path("_data/remote.directory.xp.lnk")
 
 
 @pytest.fixture
 def win7_local_lnk_dir() -> Path:
-    return absolute_path("data/local.directory.seven.lnk")
+    return absolute_path("_data/local.directory.seven.lnk")
 
 
 @pytest.fixture
 def win81_downloads_lnk_dir() -> Path:
-    return absolute_path("data/downloads.win81.lnk")
+    return absolute_path("_data/downloads.win81.lnk")
 
 
 @pytest.fixture
 def vista_idlist_lnk_file() -> Path:
-    return absolute_path("data/vista.idlist.lnk")
+    return absolute_path("_data/vista.idlist.lnk")
